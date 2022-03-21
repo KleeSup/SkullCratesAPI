4f112b5e234dd170f24703e364c0755fa108f68e
KleeSup <45318360+KleeSup@users.noreply.github.com>
2022-03-21 21:41:33 +0100

    Initial commit

---
+++ src/main/java/de/kleesup/minecraft/skullcrates/api/SkullCratesAPI.java
@@ -0,0 +1,17 @@
+package de.kleesup.minecraft.skullcrates.api;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkullCratesAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
